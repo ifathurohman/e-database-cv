360d65ddc711c60ff1ac5da0bf5d3f0f34cececf
I Fathurohman <faturahman.ilham@gmail.com>
2022-08-10 11:01:39 +0700

    update

--- asset/general_site.js
+++ asset/general_site.js
@@ -7,6 +7,6 @@ $(document).ready(function () {
     AppVersionCode: '',
     AppVersionName: '',
     SiteLogo:
-      'https://e-database-cv.herokuapp.com/public_html_a/img/attachment/alpa_SiteLogo_general_1630651094.png',
+      'https://e-database-cv.herokuapp.com/img/attachment/alpa_SiteLogo_general_1630651094.png',
   };
 });
